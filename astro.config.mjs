b30275a8aa7b8d8898e4ef72f219e0ff78bf76a0
Serhii <serhii@Serhiis-MacBook-Pro.local>
2026-04-06 18:30:18 +0300

    Page update #16

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
 						{ label: 'Miscellaneous links', slug: 'guides/dev_links' },
 						{ label: 'Unix tools', slug: 'guides/dev_doc' },
 						{ label: 'Encoding (Charsets)', slug: 'guides/encoding_doc' },
+						{ label: 'Andoid apk debugging', slug: 'guides/android_apk_inspect' },
 
 					],
 				},
